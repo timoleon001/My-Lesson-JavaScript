84c6d4af92a30881c9f4d86b9828a8898968d886
Lisov Dmitriy <mod.dmitriy@gmail.com>
2019-12-21 22:03:24 +0200

    first commit

---
+++ lesson01/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let person = {
+  name: 'John',
+  age: 25,
+  isMaried: false
+};
+
+console.log(person.name);
+
+let arr = ['plum.png', 'orage.bmp', 'apple.bmp'];
+
+console.log(arr[1])
