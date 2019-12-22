3389e16540ef86f6f6c4bf8dddcb6451de4fb25c
Dmitriy <mod.dmitriy@gmail.com>
2019-12-22 22:27:37 +0200

    lesson 01

--- lesson01/script.js
+++ lesson01/script.js
@@ -1,13 +1,21 @@
-'use strict';
+let money = prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-let person = {
-  name: 'John',
-  age: 25,
-  isMaried: false
+let appData = {
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
 };
 
-console.log(person.name);
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
 
-let arr = ['plum.png', 'orage.bmp', 'apple.bmp'];
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
 
-console.log(arr[1]);
+alert(appData.budget / 30);
